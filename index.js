3db8e14a07f517045f8996bc58c47945c36ca4db
Van Vo <frontendvan@outlook.com>
2022-03-04 21:07:44 +0000

    master

--- index.js
+++ index.js
@@ -5,3 +5,23 @@ Challenge:
    JavaScript.
 2. Do the same for Orc card. 
 */
+
+const heroDiv = document.getElementById('hero')
+hero.innerHTML = `
+   <div class="character-card">
+   <h4 class="name"> Wizard </h4>
+   <img class="avatar" src="images/wizard.png"/>
+   <p class="health">health: <b> 60 </b></p>
+   <div class="dice-container"><div class="dice"> 6 </div></div>
+   </div>    
+`
+
+const monsterDiv = document.getElementById('monster')
+monster.innerHTML = `
+   <div class="character-card">
+   <h4 class="name"> Orc </h4>
+   <img class="avatar" src="images/orc.png"/>
+   <p class="health">health: <b> 10 </b></p>
+   <div class="dice-container"><div class="dice"> 4 </div></div>
+   </div>
+`
