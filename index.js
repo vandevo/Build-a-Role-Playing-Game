7a3a8c322aaf71270b95966d741e89a7ee273cef
Van Vo <frontendvan@outlook.com>
2022-03-11 13:53:21 +0000

    master

--- index.js
+++ index.js
@@ -8,11 +8,9 @@ Challenge:
 
 
 function getDiceRollArray(diceCount) {
-   const newDiceRolls = []
-   for (let i = 0; i < diceCount; i++) {
-      newDiceRolls.push(Math.floor(Math.random()*6)+1); 
-   }
-   return newDiceRolls
+return new Array(diceCount).fill(0).map(function(){
+   return Math.floor(Math.random() * 6 + 1)
+})
 
 }
 
