ebccbb68ed144d8cdabe6e45a9cfd92bbdf74cec
Van Vo <frontendvan@outlook.com>
2022-03-15 19:13:13 +0000

    master

--- index.js
+++ index.js
@@ -40,21 +40,30 @@ const monster = {
 }
 
 
-function renderCharacter(data){
-   const {elementID, name, avatar, health, diceCount} = data;
-   const diceHtml = getDiceHtml(diceCount)
+function Character(data){
+   this.elementID = data.elementID;
+   this.name = data.name;
+   this.avatar = data.avatar;
+   this.health = data.health;
+   this.diceCount = data.diceCount;
 
+   this.getCharacterHtml = function(){
+      const {elementID, name, avatar, health, diceCount} = data;
+      const diceHtml = getDiceHtml(diceCount)
+   
+   
+      document.getElementById(elementID).innerHTML = `
+      <div class="character-card">
+      <h4 class="name">${name}</h4>
+      <img class="avatar" src="${avatar}"/>
+      <p class="health">health: <b> ${health} </b></p>
+      <div class="dice-container">${diceHtml}</div>
+      </div>    
+   `}
+   }
 
-   document.getElementById(elementID).innerHTML = `
-   <div class="character-card">
-   <h4 class="name">${name}</h4>
-   <img class="avatar" src="${avatar}"/>
-   <p class="health">health: <b> ${health} </b></p>
-   <div class="dice-container">${diceHtml}</div>
-   </div>    
-`}
+const wizard = new Character(hero)
+const orc = new Character(monster)
 
-renderCharacter(hero)
-renderCharacter(monster)
-
-
+wizard.getCharacterHtml()
+orc.getCharacterHtml()
