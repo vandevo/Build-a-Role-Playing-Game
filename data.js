b76ca533b37ce7d6d7ab470183aa0576514c989f
Van Vo <frontendvan@outlook.com>
2022-03-20 20:08:53 +0000

    master

---
+++ data.js
@@ -0,0 +1,16 @@
+const characterData = {
+    hero: {
+        elementId: "hero",
+        name: "Wizard",
+        avatar: "images/wizard.png",
+        health: 60,
+        diceCount: 3
+    },
+    monster: {
+        elementId: "monster",
+        name: "Orc",
+        avatar: "images/orc.png",
+        health: 10,
+        diceCount: 1
+    }
+}
