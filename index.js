d817585227f4990f4f4011e5c2b0f29976cdb427
Van Vo <frontendvan@outlook.com>
2022-03-22 12:25:49 +0000

    master

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function render() {
 }
 
 function attack(){
-   console.log("The attack button is working")
+   render()
 }
 
 document.getElementById('attack-button').addEventListener('click', attack)
