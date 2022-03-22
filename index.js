1264a01cd9f5a84f38b0c5be8c9b8bb651cc262b
Van Vo <frontendvan@outlook.com>
2022-03-22 12:21:57 +0000

    master

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ function render() {
    document.getElementById(orc.elementId).innerHTML = orc.getCharacterHtml();
 }
 
+function attack(){
+   console.log("The attack button is working")
+}
+
+document.getElementById('attack-button').addEventListener('click', attack)
 const wizard = new Character(characterData.hero)
 const orc = new Character(characterData.monster)
 render()
+
+
+
