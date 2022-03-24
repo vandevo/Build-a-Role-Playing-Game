6a2ec71d565decf2491df462e715a9bbc91a6f4c
Van Vo <frontendvan@outlook.com>
2022-03-24 11:33:04 +0000

    master

--- Character.js
+++ Character.js
@@ -1,9 +1,8 @@
-import {getDiceRollArray} from "./utils.js"
-
+import {getDiceRollArray, getDicePlaceholderHtml} from "./utils.js"
 
 function Character(data) {
     Object.assign(this, data)
-    
+    this.diceArray = getDicePlaceholderHtml(this.diceCount)
     this.getDiceHtml = function(diceCount) {
         return getDiceRollArray(diceCount).map(function(num){ 
             return  `<div class="dice">${num}</div>`
@@ -11,7 +10,7 @@ function Character(data) {
     }
  
     this.getCharacterHtml = function () {
-        const { elementId, name, avatar, health, diceCount } = this;      
+        const { elementId, name, avatar, health, diceCount, diceArray } = this;      
         let diceHtml = this.getDiceHtml(diceCount);
         
            return `
@@ -20,7 +19,7 @@ function Character(data) {
                 <img class="avatar" src="${avatar}" />
                 <div class="health">health: <b> ${health} </b></div>
                 <div class="dice-container">
-                    ${diceHtml}
+                    ${diceArray}
                 </div>
             </div>`;
     }  
