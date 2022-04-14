df4a2d15e8f4e8e499a20b0c3168d70b124c21ea
Van Vo <frontendvan@outlook.com>
2022-04-14 18:53:20 +0100

    master

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import characterData from "./data.js";
 import Character from "./Character.js"
 
-function render() {
-   document.getElementById(wizard.elementId).innerHTML = wizard.getCharacterHtml();
-   document.getElementById(orc.elementId).innerHTML = orc.getCharacterHtml();
+let monstersArray = ["orc", "demon", "goblin"];
+
+function getNewMonster(){
+   nextMonsterData = characterData[monstersArray.shift()] 
 }
+
+
 
 function attack(){
    wizard.getDiceHtml()
@@ -35,6 +38,12 @@ function endGame(){
 
 
 document.getElementById('attack-button').addEventListener('click', attack)
+
+function render() {
+   document.getElementById(wizard.elementId).innerHTML = wizard.getCharacterHtml();
+   document.getElementById(orc.elementId).innerHTML = orc.getCharacterHtml();
+}
+
 const wizard = new Character(characterData.hero)
 const orc = new Character(characterData.monster)
 render()
