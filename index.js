08a8a0fcf571e758b978bbe4ca471a5ff523c63c
Van Vo <frontendvan@outlook.com>
2022-04-20 02:05:23 +0100

    master

--- index.js
+++ index.js
@@ -1,63 +1,67 @@
-import characterData from "./data.js";
-import Character from "./Character.js"
+import characterData from './data.js'
+import Character from './Character.js'
 
-let monstersArray = ["orc", "demon", "goblin"];
+let monstersArray = ["orc", "demon", "goblin"]
 
-function getNewMonster(){
-   const nextMonsterData = characterData[monstersArray.shift()] 
-   return nextMonsterData ? new Character(nextMonsterData) : {}
+function getNewMonster() {
+    const nextMonsterData = characterData[monstersArray.shift()]
+    return nextMonsterData ? new Character(nextMonsterData) : {}
 }
 
+/*
+Challenge
+1. Change the attack function so that when a monster dies, 
+the next monster replaces it. If there are no more monsters,
+call endGame(). 
+2. Make sure that endGame() still gets called if the wizard
+is killed.
+*/
 
-
-function attack(){
-   wizard.getDiceHtml()
-   monster.getDiceHtml()
-   wizard.takeDamage(wizard.currentDiceScore)
-   monster.takeDamage(monster.currentDiceScore)
-   render()
-
-
-   if(wizard.dead){
-      endGame()
-   } else if (monster.dead){
-      if(monstersArray.length > 0){
-         monster = getNewMonster()
-         render()
-      }
-      else{
-         endGame()
-      }
-   }
+function attack() {
+    wizard.getDiceHtml()
+    monster.getDiceHtml()
+    wizard.takeDamage(monster.currentDiceScore)
+    monster.takeDamage(wizard.currentDiceScore)
+    render()
+    
+        /*change the code below this line*/
+    if(wizard.dead){
+        endGame()
+    }
+    else if(monster.dead){
+        if(monstersArray.length > 0){
+            monster = getNewMonster()
+            render()
+        }
+        else{
+            endGame()
+        }
+    }
 }
 
-function endGame(){
-   
+function endGame() {
+    const endMessage = wizard.health === 0 && monster.health === 0 ?
+        "No victors - all creatures are dead" :
+        wizard.health > 0 ? "The Wizard Wins" :
+            "The Orc is Victorious"
 
-   const endMessage = wizard.health === 0 && monster.health === 0 ? "No victors - all creatures are dead" :
-   wizard.health > 0 ? "The wizard wins!" : "The orc wins!"
-   console.log(endMessage)
-
-   const endEmoji = wizard.health > 0 ? "🔮" : "☠️"
-   document.body.innerHTML = `<div class="end-game">
-   <h2>Game Over</h2>
-   <h3>${endMessage}</h3>
-   <p class="end-emoji">${endEmoji}</p>
-</div>`
-
+    const endEmoji = wizard.health > 0 ? "🔮" : "☠️"
+    document.body.innerHTML = `
+        <div class="end-game">
+            <h2>Game Over</h2> 
+            <h3>${endMessage}</h3>
+            <p class="end-emoji">${endEmoji}</p>
+        </div>
+        `
 }
 
-
-document.getElementById('attack-button').addEventListener('click', attack)
+document.getElementById("attack-button").addEventListener('click', attack)
 
 function render() {
-   document.getElementById('hero').innerHTML = wizard.getCharacterHtml();
-   document.getElementById('monster').innerHTML = monster.getCharacterHtml();
+    document.getElementById('hero').innerHTML = wizard.getCharacterHtml()
+    document.getElementById('monster').innerHTML = monster.getCharacterHtml()
 }
 
 const wizard = new Character(characterData.hero)
 let monster = getNewMonster()
 render()
-
-
-
